a3d489ceadcc7ac8ac4d1b1d9fa4ca6d6b25974b
Royal VICTOR <rvictor@cbi-multimedia.com>
2023-10-16 23:07:38 +0200

    Functional Dependancy Injection with only controller declared in AppProvider

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -9,37 +9,33 @@ import AuthControllerInterface from './AuthControllerInterface'
 const acceptLanguage = require('accept-language-parser')
 
 @inject()
-export class AuthController implements AuthControllerInterface {
+export default class AuthController implements AuthControllerInterface {
   private authService: AuthService
   constructor(authService: AuthService) {
-    console.log('ici')
     this.authService = authService
-    console.log('authService :>> ', authService)
   }
 
   public async register({ request, response }: HttpContextContract) {
-    console.log('request :>> ', request)
-    console.log('response :>> ', response)
-    // try {
-    //   // Valider les données de la requête en utilisant le validateur prestationUpdate
-    //   const data = await request.validate(CreateUserValidator)
+    try {
+      // Valider les données de la requête en utilisant le validateur prestationUpdate
+      const data = await request.validate(CreateUserValidator)
 
-    //   const acceptLangHeader = request.header('Accept-Language')
-    //   const languages = acceptLanguage.parse(acceptLangHeader)
+      const acceptLangHeader = request.header('Accept-Language')
+      const languages = acceptLanguage.parse(acceptLangHeader)
 
-    //   const userData: UserCreateDTO = {
-    //     email: data.email,
-    //     password: data.password,
-    //     user_language: languages[0],
-    //     internal_or_sso: AuthentificationMode.INTERNAL,
-    //   }
+      const userData: UserCreateDTO = {
+        email: data.email,
+        password: data.password,
+        user_language: languages[0],
+        internal_or_sso: AuthentificationMode.INTERNAL,
+      }
 
-    //   const user = await this.authService.createUserAccount(userData)
-    //   return response.created(user) // 201 CREATED
-    // } catch (error) {
-    //   console.log('error :>> ', error)
-    //   // Gérer les erreurs de validation en utilisant les messages d'erreur traduits
-    //   return response.status(400).json({ errors: error.messages })
-    // }
+      const user = await this.authService.createUserAccount(userData)
+      return response.created(user) // 201 CREATED
+    } catch (error) {
+      console.log('error :>> ', error)
+      // Gérer les erreurs de validation en utilisant les messages d'erreur traduits
+      return response.status(400).json({ errors: error.messages })
+    }
   }
 }
--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -1,19 +1,19 @@
 import type { ApplicationContract } from '@ioc:Adonis/Core/Application'
-import { AuthController } from 'App/Controllers/Http/AuthController'
 
 export default class AppProvider {
   constructor(protected app: ApplicationContract) {}
 
   public async register() {
     // Register your own bindings
+
+    // Le default : AuthController a été très important pour que le container puisse instancier la classe
+    // et que la méthode register soit trouvée
+    const { default: AuthController } = await import('../app/Controllers/Http/AuthController')
+    this.app.container.make(AuthController)
   }
 
   public async boot() {
     // IoC container is ready
-    const { AuthController } = await import('../app/Controllers/Http/AuthController')
-    const authController = this.app.container.make(AuthController)
-
-    console.log('authController :>> ', authController)
   }
 
   public async ready() {
