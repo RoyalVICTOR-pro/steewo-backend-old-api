7321a4beb27a1916829e575cd59b02c8eae357ff
Royal VICTOR <rvictor@cbi-multimedia.com>
2023-10-19 17:04:13 +0200

    Profession Creation Validator and Errors Management

--- app/Controllers/Http/ProfessionsController.ts
+++ app/Controllers/Http/ProfessionsController.ts
@@ -1,6 +1,7 @@
 import { inject } from '@adonisjs/core/build/standalone'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { ProfessionService } from 'App/Services/ProfessionService'
+import ProfessionCreateOrUpdateValidator from 'App/Validators/ProfessionCreateOrUpdateValidator'
 
 @inject()
 export default class ProfessionsController {
@@ -10,29 +11,67 @@ export default class ProfessionsController {
   }
 
   public async getAllProfessions({ response }: HttpContextContract) {
-    const professions = await this.professionService.listProfessions()
-    return response.ok(professions)
+    try {
+      const professions = await this.professionService.listProfessions()
+      return response.ok(professions)
+    } catch (error) {
+      error.message = 'While getting all professions : ' + error.message
+      console.log(error)
+      return response.status(500).json({ message: 'Internal Server Error' })
+    }
   }
 
   public async getProfessionById({ response, params }: HttpContextContract) {
-    const profession = await this.professionService.getProfessionById(params.id)
-    return response.ok(profession)
+    try {
+      const profession = await this.professionService.getProfessionById(params.id)
+      return response.ok(profession)
+    } catch (error) {
+      error.message = 'While getting profession ' + params.id + ' : ' + error.message
+      console.log(error)
+      return response.status(500).json({ message: 'Internal Server Error' })
+    }
   }
 
   public async createProfession({ request, response }: HttpContextContract) {
-    const data = request.only(['name_fr'])
-    const profession = await this.professionService.createProfession(data)
-    return response.created(profession)
+    try {
+      // TODO : S'occuper de l'upoad des images
+      const data = await request.validate(ProfessionCreateOrUpdateValidator)
+      const newProfession = {
+        name_fr: data.name_fr,
+        picto_path: data.picto_path,
+        image_path: data.image_path,
+        is_enabled: data.is_enabled,
+      }
+
+      const profession = await this.professionService.createProfession(newProfession)
+      return response.created(profession)
+    } catch (error) {
+      error.message = 'While creating a profession : ' + error.message
+      console.log(error)
+      return response.status(500).json({ message: 'Internal Server Error' })
+    }
   }
 
   public async updateProfession({ request, response }: HttpContextContract) {
     const data = request.only(['id', 'name_fr'])
-    const profession = await this.professionService.updateProfession(data)
-    return response.ok(profession)
+    try {
+      const profession = await this.professionService.updateProfession(data)
+      return response.ok(profession)
+    } catch (error) {
+      error.message = 'While updating profession ' + data.id + ' : ' + error.message
+      console.log(error)
+      return response.status(500).json({ message: 'Internal Server Error' })
+    }
   }
 
   public async deleteProfession({ response, params }: HttpContextContract) {
-    await this.professionService.deleteProfession(params.id)
-    return response.noContent()
+    try {
+      await this.professionService.deleteProfession(params.id)
+      return response.noContent()
+    } catch (error) {
+      error.message = 'While deleting profession ' + params.id + ' : ' + error.message
+      console.log(error)
+      return response.status(500).json({ message: 'Internal Server Error' })
+    }
   }
 }
---
+++ app/Validators/ProfessionCreateOrUpdateValidator.ts
@@ -0,0 +1,21 @@
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class ProfessionCreateOrUpdateValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    name_fr: schema.string({}, [
+      rules.required(),
+      rules.unique({ table: 'professions', column: 'name_fr' }),
+    ]),
+    picto_path: schema.string(),
+    image_path: schema.string(),
+    is_enabled: schema.boolean(),
+  })
+
+  public messages: CustomMessages = {
+    'name_fr.required': 'Le nom de la profession est requis.',
+    'name_fr.unique': 'Cette profession existe déjà.',
+  }
+}
