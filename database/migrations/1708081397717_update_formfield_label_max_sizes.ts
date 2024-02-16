791eebb9a36b14549763611a4442aedf166db4bf
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-02-16 12:09:07 +0100

    Form Field Label Max Size Update Migration

---
+++ database/migrations/1708081397717_update_formfield_label_max_sizes.ts
@@ -0,0 +1,17 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'services_form_fields'
+
+  public async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('label', 255).nullable().alter()
+    })
+  }
+
+  public async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('label', 100).nullable().alter()
+    })
+  }
+}
