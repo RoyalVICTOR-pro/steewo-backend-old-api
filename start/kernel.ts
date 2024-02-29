a68b941a42dfdc4a87731a519ad6d7192a2c4511
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-02-29 11:18:22 +0100

    VS Code Issue Quick Fix

--- start/kernel.ts
+++ start/kernel.ts
@@ -38,7 +38,7 @@ Server.middleware.register([
 |
 | and then use it as follows
 |
-| Route.get('dashboard', 'UserController.dashboard').middleware('auth')
+| Route.get('logout', 'AuthController.logout').middleware('auth')
 |
 */
 Server.middleware.registerNamed({
