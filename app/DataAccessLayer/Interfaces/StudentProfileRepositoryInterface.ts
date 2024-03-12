1bc21b98130eb686d93f22a99c1948efbc1079cc
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-03-12 15:19:44 +0100

    Quick Clean : Update of Interfaces

--- app/DataAccessLayer/Interfaces/StudentProfileRepositoryInterface.ts
+++ app/DataAccessLayer/Interfaces/StudentProfileRepositoryInterface.ts
@@ -1,8 +1,28 @@
 import StudentProfile from '@Models/StudentProfile'
+import StudentProfileBannerUpdateDTO from '@DTO/StudentProfileBannerUpdateDTO'
 import StudentProfileCreateDTO from '@DTO/StudentProfileCreateDTO'
+import StudentProfileDescriptionUpdateDTO from '@DTO/StudentProfileDescriptionUpdateDTO'
+import StudentProfileMainUpdateDTO from '@DTO/StudentProfileMainUpdateDTO'
+import StudentProfilePhotoUpdateDTO from '@DTO/StudentProfilePhotoUpdateDTO'
 
 export default interface StudentProfileRepositoryInterface {
   createStudentProfile(data: StudentProfileCreateDTO): Promise<StudentProfile>
   getStudentProfileByEmail(email: string): Promise<StudentProfile | null>
   getStudentProfileByUserId(userId: number): Promise<StudentProfile | null>
+  updateStudentProfileMainInfo(
+    userId: number,
+    data: StudentProfileMainUpdateDTO
+  ): Promise<StudentProfile>
+  updateStudentProfileDescription(
+    userId: number,
+    data: StudentProfileDescriptionUpdateDTO
+  ): Promise<StudentProfile>
+  updateStudentProfilePhoto(
+    userId: number,
+    data: StudentProfilePhotoUpdateDTO
+  ): Promise<StudentProfile>
+  updateStudentProfileBanner(
+    userId: number,
+    data: StudentProfileBannerUpdateDTO
+  ): Promise<StudentProfile>
 }
--- app/Services/Interfaces/StudentProfileServiceInterface.ts
+++ app/Services/Interfaces/StudentProfileServiceInterface.ts
@@ -1,6 +1,23 @@
+import { MultipartFileContract } from '@ioc:Adonis/Core/BodyParser'
 import StudentProfile from '@Models/StudentProfile'
 import StudentProfileCreateDTO from '@DTO/StudentProfileCreateDTO'
+import StudentProfileMainUpdateDTO from '@DTO/StudentProfileMainUpdateDTO'
 
 export default interface StudentProfileServiceInterface {
   createStudentProfile(data: StudentProfileCreateDTO): Promise<StudentProfile>
+  getStudentPublicProfile(user_id: number): Promise<any>
+  getStudentPrivateProfile(user_id: number): Promise<any>
+  updateStudentProfileMainInfo(
+    userId: number,
+    data: StudentProfileMainUpdateDTO
+  ): Promise<StudentProfile>
+  updateStudentProfileDescription(userId: number, description: string): Promise<StudentProfile>
+  updateStudentProfilePhoto(
+    userId: number,
+    photo_file: MultipartFileContract | null
+  ): Promise<StudentProfile>
+  updateStudentProfileBanner(
+    userId: number,
+    banner_file: MultipartFileContract | null
+  ): Promise<StudentProfile>
 }
