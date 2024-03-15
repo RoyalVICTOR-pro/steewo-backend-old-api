5f3244ffb62c22181fda754fdc915715a82e9d4d
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-03-15 14:18:32 +0100

    Improvment : Filter Profile Owners by Profile ID and User ID

--- app/Middleware/IsClientProfileOwner.ts
+++ app/Middleware/IsClientProfileOwner.ts
@@ -1,10 +1,19 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import ClientProfile from '@Models/ClientProfile'
 
 export default class IsClientProfileOwner {
   public async handle({ auth, params, response }: HttpContextContract, next: () => Promise<void>) {
     const user = auth.user
-    if (user?.id !== Number(params.user_id)) {
-      return response.unauthorized({ message: 'You can only access your own profile.' })
+    if (params.user_id) {
+      if (user?.id !== Number(params.user_id)) {
+        return response.unauthorized({ message: 'You can only access your own profile.' })
+      }
+    }
+    if (params.client_profile_id) {
+      const clientProfile = await ClientProfile.findBy('id', params.client_profile_id)
+      if (user?.id !== clientProfile?.user_id) {
+        return response.unauthorized({ message: 'You can only access your own profile.' })
+      }
     }
     await next()
   }
--- app/Middleware/IsStudentProfileOwner.ts
+++ app/Middleware/IsStudentProfileOwner.ts
@@ -1,10 +1,19 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import StudentProfile from '@Models/StudentProfile'
 
 export default class IsStudentProfileOwner {
   public async handle({ auth, params, response }: HttpContextContract, next: () => Promise<void>) {
     const user = auth.user
-    if (user?.id !== Number(params.user_id)) {
-      return response.unauthorized({ message: 'You can only access your own profile.' })
+    if (params.user_id) {
+      if (user?.id !== Number(params.user_id)) {
+        return response.unauthorized({ message: 'You can only access your own profile.' })
+      }
+    }
+    if (params.student_profile_id) {
+      const studentProfile = await StudentProfile.findBy('id', params.student_profile_id)
+      if (user?.id !== studentProfile?.user_id) {
+        return response.unauthorized({ message: 'You can only access your own profile.' })
+      }
     }
     await next()
   }
