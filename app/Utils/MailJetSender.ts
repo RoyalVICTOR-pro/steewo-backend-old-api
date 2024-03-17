2620040028405253ffe4b4ad007a54e10a3309d6
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-03-17 23:26:35 +0100

    In Progress : MailJet Sender

---
+++ app/Utils/MailJetSender.ts
@@ -0,0 +1,17 @@
+export class MailJetSender {
+  // public static async send(data: {
+  //   from: string
+  //   to: string
+  //   subject: string
+  //   html_view: string
+  //   variables_for_view: object
+  // }) {
+  //   await Mail.send((message) => {
+  //     message
+  //       .from(data.from)
+  //       .to(data.to)
+  //       .subject(data.subject)
+  //       .htmlView(data.html_view, data.variables_for_view)
+  //   })
+  // }
+}
