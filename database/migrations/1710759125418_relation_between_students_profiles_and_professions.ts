e89f528c311df55d83681d6c8210871cced08270
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-03-18 12:06:52 +0100

    In Progress : Professions and services of a student (Migrations)

---
+++ database/migrations/1710759125418_relation_between_students_profiles_and_professions.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'student_profiles_has_professions'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('student_profile_id').unsigned().references('student_profiles.id')
+      table.integer('profession_id').unsigned().references('professions.id')
+      table.index(['student_profile_id', 'profession_id'], 'student_profession_relation')
+      table.tinyint('has_been_accepted').defaultTo(0)
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1710759544916_relation_between_students_profiles_and_services.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'student_profiles_has_services'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('student_profile_id').unsigned().references('student_profiles.id')
+      table.integer('service_id').unsigned().references('services.id')
+      table.index(['student_profile_id', 'service_id'], 'student_service_relation')
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
