7d6c40fc335337821d0f0d66b0ee43505206b677
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-03-20 15:29:12 +0100

    In Progress : Achievements Management (Validators Update)

--- app/Validators/AchievementAddValidator.ts
+++ app/Validators/AchievementAddValidator.ts
@@ -1,4 +1,4 @@
-import { schema, CustomMessages } from '@ioc:Adonis/Core/Validator'
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Config from '@ioc:Adonis/Core/Config'
 
@@ -6,21 +6,43 @@ export default class AchievementAddOrUpdateValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    service_id: schema.number(),
-    title: schema.string.optional(),
+    service_id: schema.number([rules.exists({ table: 'services', column: 'id' })]),
+    title: schema.string(),
     description: schema.string.optional(),
     context: schema.string.optional(),
     date: schema.date.optional(),
     isFavorite: schema.boolean.optional(),
-    main_image_file: schema.file.optional({
+    main_image_file: schema.file({
       size: Config.get('custom.MAX_IMAGE_FILE_SIZE'),
       extnames: Config.get('custom.IMAGE_FILE_TYPES'),
     }),
+    achievement_details: schema.array.optional().members(
+      schema.file.optional({
+        size: Config.get('custom.MAX_DOCUMENT_FILE_SIZE'),
+        extnames: [
+          ...Config.get('custom.IMAGE_FILE_TYPES'),
+          ...Config.get('custom.DOCUMENT_FILE_TYPES'),
+          ...Config.get('custom.MEDIA_FILE_TYPES'),
+        ],
+      })
+    ),
   })
 
   public messages: CustomMessages = {
     'service_id.required': 'Vous devez spécifier un service.',
     'title.string': 'Le titre doit être une chaîne de caractères.',
+    'achievement_details.*.size':
+      'La taille du fichier ne doit pas dépasser la taille maximale autorisée (' +
+      Config.get('custom.MAX_DOCUMENT_FILE_SIZE') +
+      ').',
+    'achievement_details.*.extnames':
+      'Le fichier doit avoir une extension de fichier valide (' +
+      [
+        ...Config.get('custom.IMAGE_FILE_TYPES'),
+        ...Config.get('custom.DOCUMENT_FILE_TYPES'),
+        ...Config.get('custom.MEDIA_FILE_TYPES'),
+      ].join(',') +
+      ').',
     'description.string': 'La description doit être une chaîne de caractères.',
     'context.string': 'Le contexte doit être une chaîne de caractères.',
     'date.date': 'La date doit être une date.',
