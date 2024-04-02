ee85c7d5b5cb7e1fa487fa9dc037fc74061ea2e4
Royal VICTOR <rvictor@cbi-multimedia.com>
2024-04-02 09:18:38 +0200

    Refacto : Renaming of ClientsController

--- start/routes/clients.ts
+++ start/routes/clients.ts
@@ -2,36 +2,46 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   // CLIENTS
-  Route.post('/client/register-individual', 'ClientsController.createIndividualClientProfile').as(
-    'createIndividualClientProfile'
-  )
+  Route.post(
+    '/client/register-individual',
+    'ClientsProfileController.createIndividualClientProfile'
+  ).as('createIndividualClientProfile')
   Route.post(
     '/client/register-professional',
-    'ClientsController.createProfessionnalClientProfile'
+    'ClientsProfileController.createProfessionnalClientProfile'
   ).as('createProfessionnalClientProfile')
-  Route.get('/client/get-public-profile/:user_id', 'ClientsController.getClientPublicProfile')
+  Route.get(
+    '/client/get-public-profile/:user_id',
+    'ClientsProfileController.getClientPublicProfile'
+  )
     .as('getClientPublicProfile')
     .middleware(['auth', 'isValidEmail'])
-  Route.get('/client/get-private-profile/:user_id', 'ClientsController.getClientPrivateProfile')
+  Route.get(
+    '/client/get-private-profile/:user_id',
+    'ClientsProfileController.getClientPrivateProfile'
+  )
     .as('getClientPrivateProfile')
     .middleware(['auth', 'isClientProfileOwner', 'isValidEmail'])
   Route.patch(
     '/client/update-profile/:user_id/main',
-    'ClientsController.updateClientProfileMainInfo'
+    'ClientsProfileController.updateClientProfileMainInfo'
   )
     .as('updateClientProfileMainInfo')
     .middleware(['auth', 'isClientProfileOwner', 'isValidEmail'])
-  Route.patch('/client/update-profile/:user_id/photo', 'ClientsController.updateClientProfilePhoto')
+  Route.patch(
+    '/client/update-profile/:user_id/photo',
+    'ClientsProfileController.updateClientProfilePhoto'
+  )
     .as('updateClientProfilePhoto')
     .middleware(['auth', 'isClientProfileOwner', 'isValidEmail'])
 
   Route.patch(
     '/client/update-profile/:user_id/description',
-    'ClientsController.updateClientProfileDescription'
+    'ClientsProfileController.updateClientProfileDescription'
   )
     .as('updateClientProfileDescription')
     .middleware(['auth', 'isClientProfileOwner', 'isValidEmail'])
-  Route.patch('/client/accept-charter/:user_id', 'ClientsController.acceptClientCharter')
+  Route.patch('/client/accept-charter/:user_id', 'ClientsProfileController.acceptClientCharter')
     .as('acceptClientCharter')
     .middleware(['auth', 'isClientProfileOwner', 'isValidEmail'])
 }).prefix('/v1')
